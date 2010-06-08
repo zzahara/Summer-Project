18b7e42988d939f9f733eec5602e9a74bca13585
zdocena <zahara.docena@gmail.com>
2010-06-08 09:58:54 -0700

    clean up

--- processLogFile.py
+++ processLogFile.py
@@ -2,64 +2,67 @@ import re
 from sys import argv
 
 script, filename, image_src = argv
-pages = dict()
+pages = dict() # keys = url : value = Page object
 
 class Page():
     ave_load = 0;
     standard_dev = 0;
     num_visits = 1;
 
-    
+    def set_values(ave, sd, visits):
+        ave_load = ave
+        standard_dev = sd
+        num_visits = visits
+
+    # doesn't work when printing elements in hashmap?  
     def __str__():
         return str(self.ave_load)
-
 
 
 # open and read access.log file
 def process_file(filename, img_src):
     
     file = open(filename, "r");
-
     line = file.readline();
+
     for line in file:
 
-        #print line
         # get request
-        split_list = line.split('\"');
-        request = (split_list[1].split(' '))[1];
-        
+        split_log = line.split('\"');
+        request = (split_log[1].split(' '))[1];
 
-        if (request.startswith(img_src)):
-            #print split_list
-
-            # get referer, if there is one
-            if (len(split_list) > 3):
-                #print request
-                referer = split_list[3]
-                #print referer + '\n'
-                if (referer != '-' ): update_page(referer, 1)
+        # get referer and update pages hashmap
+        # only process this log if the http request is for the load time image
+        if (request.startswith(img_src) and len(split_log) > 3):
+            referer = split_log[3]
+            if (referer != '-' ): update_page(referer, 1)
 
     file.close()
 
-# store referer and other info     
-def update_page(referer, new_loadtime):
-   
-    # if in list update values
-    if (pages.has_key(referer)):
-        data = pages[referer]
-        ave_load = data.ave_load
-        num_visits = data.num_visits
 
-        data.ave_load = ((num_visits * ave_load) + new_loadtime)/ (num_visits + 1)
-        data.num_visits = num_visits + 1
-        data.standard_dev = 0
+# store or update page values     
+def update_page(url, new_loadtime):
+   
+    # if page is already in list update values
+    if (pages.has_key(url)):
+        value = pages[url]
+        ave_load = value.ave_load
+        num_visits = value.num_visits
+
+        value.ave_load = ((num_visits * ave_load) + new_loadtime)/ (num_visits + 1)
+        value.num_visits = num_visits + 1
+        value.standard_dev = 0
 
     # else add page to list
     else:
         new_page = Page()
+
+        # results in TypeError: set_values() takes exactly 3 arguments (4 given) ????
+        # new_page.set_values(new_loadtime, 0, 1) 
+
         new_page.ave_load = new_loadtime
         new_page.standard_dev = 0
-        pages[referer] = new_page
+        pages[url] = new_page
 
 
 def print_pages():
@@ -69,7 +72,5 @@ def print_pages():
 process_file(filename, image_src)
 print_pages()
 
-
-print "End of Program"
 
 
