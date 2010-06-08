4b7b517a04135388d2488dc445a5444a8982b729
zdocena <zahara.docena@gmail.com>
2010-06-08 15:56:09 -0700

    calculates standard deviation

--- processLogFile.py
+++ processLogFile.py
@@ -1,4 +1,5 @@
 import re
+import math
 from sys import argv
 
 script, filename, image_src = argv
@@ -8,21 +9,27 @@ class Page():
     ave_load = 0;
     standard_dev = 0;
     num_visits = 1;
+    loadtimes = []
 
     def set_values(self, ave_load, standard_dev, visits):
         self.ave_load = ave_load
         self.standard_dev = standard_dev
         self.num_visits = visits
 
-    def print_values(self):
-        print "ave = " + self.ave_load + " sd = " + self.standard_dev + " visits = " + self.num_visits
-
-    # doesn't work when printing elements in hashmap?  
-    def __str__(self):
-        return str(self.num_visits)
-
-    def __int__(self):
+    def append(self,loadtime):
+        self.loadtimes.append(loadtime)
+ 
+    def get_visits(self):
         return self.num_visits
+
+    def print_values(self):
+        print "ave = " + self.ave_load + " stand_dev = " + self.standard_dev + " visits = " + self.num_visits
+
+    def print_loadtimes(self):
+        print self.loadtimes
+
+    def __str__(self):
+        return str(self.ave_load) + ": " + str(self.standard_dev) + ": " + str(self.num_visits) 
 
 
 # open and read access.log file
@@ -61,42 +68,52 @@ def update_page(url, new_loadtime):
    
     # if page is already in list update values
     if (pages.has_key(url)):
-        value = pages[url]
-        ave_load = value.ave_load
-        num_visits = value.num_visits
+        page = pages[url]
+        ave_load = page.ave_load
+        num_visits = page.num_visits
 
-        value.ave_load = ((num_visits * ave_load) + new_loadtime)/ (num_visits + 1)
-        value.num_visits = num_visits + 1
-        value.standard_dev = 0
+        page.append(new_loadtime)
+        ave_load = ((num_visits * ave_load) + new_loadtime)/ (num_visits + 1)
+        standard_dev = calc_standard_dev(page.loadtimes, ave_load)
 
-        print url + ": " + new + ", " + str(value.ave_load)
-        #print "visits = " + str(num_visits) + " ave_load = " + str(ave_load)
+        page.set_values(ave_load, standard_dev, num_visits + 1)
 
-        # DEBUGGING
-        #prev = (num_visits * ave_load)
-        #num = (num_visits * ave_load) + new_loadtime
-        #denom = (num_visits + 1)
-        #print " computing values: prev = " + str(prev) + " num = " + str(num) + " denom = " + str(denom)
+        print url + ": loadtime = " + new + "---",
+        print page
 
     # else add page to list
     else:
         new_page = Page()
+        new_page.append(new_loadtime)
         new_page.set_values(new_loadtime, 0, 1) 
         pages[url] = new_page
 
-        print url + ": " + str(new_page.ave_load) + ", " + new + "------------NEW"
+        print url + ": loadtime = " + new + "---",
+        print new_page,
+        print "------------NEW"
 
-def sort_loadtimes():
+def calc_standard_dev(list, average):
+    sum = 0
+
+    if len(list) <= 1:
+        return 0
+
+    else:
+        for x in list:
+            sum = sum + math.pow((x-average), 2)
+
+        return math.sqrt(sum/(len(list)-1))
+        
+
+def sort_by_visits():
     items = pages.items()
-
-    sorted_by_visits = [ [int(page[1]),page[0]] for page in items]
+    sorted_by_visits = [ [page[1].get_visits(),page[0]] for page in items]
     sorted_by_visits.sort(reverse=True)
-    print sorted_by_visits
 
     return sorted_by_visits
 
 # return the most visited pages
-def get_most_visited(amount, list_by_visits):
+#def get_most_visited(amount, list_by_visits):
     # will finish
      
     
@@ -112,6 +129,6 @@ def print_pages():
 # main
 process_file(filename, image_src)
 #print_pages()
-sort_loadtimes()
+sort_by_visits()
 
 
