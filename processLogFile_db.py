6340e273a300a0850b0f454fd576823dda19fbc0
zdocena <zahara.docena@gmail.com>
2010-06-15 16:02:34 -0700

    processLogFile_db.py

--- processLogFile_db.py
+++ processLogFile_db.py
@@ -9,32 +9,30 @@ connect = sqlite3.connect('pages.sqlite')
 c = connect.cursor()
 next_id = 0
 
-fields = {'user':'STRING', 'page':'STRING', 'loadtime':'INTEGER', 'locale':'STRING', 'referer':'STRING', 'timestamp':'INTEGER', 'useragent':'STRING'}
+table_cols = {'user':'STRING', 'page':'STRING', 'loadtime':'INTEGER', 'locale':'STRING', 'referer':'STRING', 'timestamp':'INTEGER', 'useragent':'STRING'}
 
 def init():
-    # delete table and index
+    # delete table and indicies
+    keys = table_cols.keys()
     c.execute(""" DROP TABLE IF EXISTS pages;""")
-    c.execute(""" DROP INDEX IF EXISTS pages_index;""")
+    for col in keys:
+       c.execute("DROP INDEX IF EXISTS " + col + "_index;")
 
     # create string for fields and types
     count = 0
-    values = ''
-    keys = fields.keys()
+    columns = []
     
-    for field in keys:
-        values = values + field + ' ' + fields[field]
-
-        if count < len(keys)-1:
-            values = values + ', '
-        count = count + 1
-
-    # create table
-    c.execute("CREATE TABLE IF NOT EXISTS pages(" + values + ");")
-
-    # create an index for each column
-    for field in keys:
-       c.execute("CREATE UNIQUE INDEX IF NOT EXISTS " + field + "_index ON pages(" + field + ");")
+    for col in keys:
+        col_type = col + ' ' + table_cols[col]
+        columns.append(col_type)
+    values = ', '.join(columns)
         
+
+    # create table and an index for each column
+    c.execute("CREATE TABLE IF NOT EXISTS pages(" + values + ");")
+    for col in keys:
+       c.execute("CREATE INDEX IF NOT EXISTS " + col+ "_index ON pages(" + col + ");")
+
     connect.commit()
 
 def get_input():
@@ -42,24 +40,45 @@ def get_input():
     image_src = rawinput("Enter image source: ")
     num_top_pages = rawinput("Enter the number of top pages: ")
 
-def process_file2(filename, img_src):
+def process_file(filename, img_src):
     request = ''
     file = open(filename, "r")
 
     # referer must be 2 or more chars to avoid "-" referers
-    pattern = '(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) [^ ]+ [^ ]+ \[(?P<date>../.../....:..:..:.. .....)\] ' + '"GET (?P<bug>' + img_src + '[^"]+)" (?P<status>\d{3}) (?P<size>[^ ]+) "(?P<referer>[^"]{2,})" "(?P<useragent>[^"]+)"'
+    pattern = '(?P<user>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) [^ ]+ [^ ]+ \[(?P<timestamp>../.../....:..:..:.. .....)\] ' + '"GET (?P<bug>' + img_src + '[^"]+)" (?P<status>\d{3}) (?P<size>[^ ]+) "(?P<page>[^"]{2,})" "(?P<useragent>[^"]+)"'
     c = re.compile(pattern)
 
-    for line in file:
-        m = c.match(line)
+    for log_line in file:
+        m = c.match(log_line)
         if m:
-            event = m.groupdict()
-            loadtime = get_loadtime(event['bug'], img_src)
+            page_data = m.groupdict()
+            bug_values = get_values(page_data['bug'], img_src)
+
+            page_data.update(bug_values)
             
+            #print page_data
+            update_database(page_data)
+    file.close()
+            
+def get_values(bug, img_src):
+    end = bug.replace(img_src + '?', '')
+    bug_values = dict(item.split("=") for item in end.split("&"))
+    return bug_values
+
+def update_database(page_values):
+    keys = table_cols.keys()
+    values = []
+
+    for col in keys:
+        print col + ':'
+        print page_values[col]
+        values.append(page_values[col])
         
+    c.execute("""INSERT INTO pages values (?,?,?,?,?,?,?)""", (values[0], values[1], values[2], values[3], values[4], values[5], values[6]))
+    connect.commit()
 
 # read log file, parse out loadtimes and urls, and store in database
-def process_file(filename, img_src):
+def process_file2(filename, img_src):
     request = ''
     file = open(filename, "r");
 
@@ -86,7 +105,7 @@ def get_loadtime(request, img_src):
     return re.sub(pattern, '', request)
 
 # store a new page into the database
-def update_database(referer, loadtime):
+def update_database2(referer, loadtime):
     global next_id
     c.execute("""SELECT id FROM pages where url = ?""", (referer,))
     id = c.fetchone()
@@ -149,8 +168,8 @@ def print_database():
     for row in c:
         print row
     
-#init()        
-process_file2(filename, image_src)
+init()        
+process_file(filename, image_src)
 #process_database()
 
 c.close()
