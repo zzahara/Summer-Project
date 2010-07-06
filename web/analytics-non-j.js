4bfa76233b20c15e9f3027c09d58e1147d8bf5b4
zdocena <zahara.docena@gmail.com>
2010-07-06 14:48:32 -0700

    non jquery version

---
+++ web/analytics-non-j.js
@@ -0,0 +1,56 @@
+/* 
+* Contact Info:
+* Zahara Docena
+* zahara.docena@gmail.com
+* zahara@archive.org
+*/
+
+var startTime = new Date();
+var loadTime = 0.0;
+img_src = "analytics.archive.org/0.gif"
+
+
+window.onload = function() {
+    var endTime = new Date();
+    values = []
+    values['loadTime'] = ((endTime.getTime() - startTime.getTime())/100)/10;
+
+    // Get field values    
+    values['timeDiff'] = (new Date().getTimezoneOffset()/60)*(-1); 
+    values['locale'] = get_locale();
+    values['referrer'] = document.referrer
+
+    string = format_bug(values);
+    console.log(string)
+
+    loadtime_img = new Image(100,25);
+    loadtime_img.src = img_src + "?" + string;
+};
+
+
+function format_bug(values) {
+    ret = []
+    for (var data in values)
+      ret.push(encodeURIComponent(data) + "=" + encodeURIComponent(values[data]));
+
+    return ret.join("&")
+};
+
+function get_locale() {
+    if (navigator) {
+        if (navigator.language)
+            return navigator.language;
+            
+        else if (naviagtor.browserLanguage)
+            return navigator.browserLanguage;
+
+        else if (navigator.systemLanguage) 
+            return navigator.systemLanguage;
+
+        else if (navigator.userLanguage)
+            return navigator.userLanguage;
+    }
+
+};
+
+
