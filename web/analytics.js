991231f7985515c7d644673b3add599da42a6168
zdocena <zahara.docena@gmail.com>
2010-07-06 14:24:11 -0700

    cleaned up to go live

--- web/analytics.js
+++ web/analytics.js
@@ -1,39 +1,40 @@
+/* 
+* Contact Info:
+* Zahara Docena
+* zahara.docena@gmail.com
+* zahara@archive.org
+*/
+
 var startTime = new Date();
 var loadTime = 0.0;
-img_src = "http://analytics.archive.org/0.gif"
+img_src = "analytics.archive.org/0.gif"
 
 $(document).ready(function() {
     alert("Taking up some load time : )");
+
     var endTime = new Date();
-    loadTime = ((endTime.getTime() - startTime.getTime())/100)/10;
+    values = []
+    values['loadTime'] = ((endTime.getTime() - startTime.getTime())/100)/10;
 
-    // Get field values
-    timeDiff = (new Date().getTimezoneOffset()/60)*(-1); 
-    locale = get_locale();
-    referrer = document.referrer
-    
+    // Get field values    
+    values['timeDiff'] = (new Date().getTimezoneOffset()/60)*(-1); 
+    values['locale'] = get_locale();
+    values['referrer'] = document.referrer
+
+    string = format_bug(values);
+    console.log(string)
+
     loadtime_img = new Image(100,25);
-    loadtime_img.src = img_src + "?loadtime=" + loadTime;
-    
-    format_bug(loadTime, locale, timeDiff);
-
-    // Console Logging
-    console.log("Loadtime = " + loadTime);
-    console.log("Time Difference = " + timeDiff);
-    console.log("Locale = " + locale);
-    console.log("Referrer = " + referrer);
+    loadtime_img.src = img_src + "?" + string;
 });
 
-function format_bug(loadtime, locale, timediff) {
-    var values = {'loadtime': loadtime, 'locale': locale, 'timediff': timeDiff};
 
+function format_bug(values) {
     ret = []
     for (var data in values)
       ret.push(encodeURIComponent(data) + "=" + encodeURIComponent(values[data]));
-   console.log(img_src + "?" + ret.join("&"));
-    
-    
-    
+
+    return ret.join("&")
 };
 
 function get_locale() {
@@ -53,20 +54,4 @@ function get_locale() {
 
 };
 
-// args should be an array
-function format_request(loadtime, locale, timediff) {
-
-    values = "loadtime=" + loadtime + "&locale=" + escape(locale) + "&timediff=" + escape(timediff);
-    src = img_src + "?" + escape(values);
-    console.log("src = " + src)
-    //document.write(src);
-
-    var data = { 'loadtime': 578, 'locale': 'en-US'};
-
-   var ret = [];
-   for (var d in data)
-      ret.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));
-   console.log(ret.join("&"));
-
-};
 
