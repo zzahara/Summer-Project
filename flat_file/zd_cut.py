c0c5e8b69871d9f718757eac9931acad08c34ec9
zdocena <zahara.docena@gmail.com>
2010-07-08 14:16:02 -0700

    uses stdin

--- flat_file/zd_cut.py
+++ flat_file/zd_cut.py
@@ -11,52 +11,55 @@ from sys import argv
 from optparse import OptionParser
 
 argv
-filename = ''
 parser = OptionParser()
 cut_fields_str = []
 
 def process_args():
     global argv, cut_fields_str, filename
     parser.add_option("-f", action="append", dest="fields")
-    parser.add_option("--file", action="store", dest = "file")
 
     (options, args) = parser.parse_args(argv)
     cut_fields_str = options.fields
-    print cut_fields_str
-    
-    if options.file != None:
-        filename = options.file
-        return open(options.file, "r")
 
-    else:
-        filename = sys.stdin
-        return sys.stdin
 
-def get_field_nums(file):
+def get_script_args():
 
-    first_line = file.readline()
-    field_names = first_line.split('\t')
-    print field_names
+    field_names = get_field_list()
+    field_nums = ''
 
-    cut_fields_int = ''
+    script_args = ['']
 
     for i in range(0, len(cut_fields_str)):
         index = field_names.index(cut_fields_str[i])
-        cut_fields_int = cut_fields_int + str(index+1)
+        field_nums = field_nums + str(index+1)
         
         if i != len(cut_fields_str)-1:
-            cut_fields_int = cut_fields_int + ','
+            field_nums = field_nums + ','
 
-    return cut_fields_int
+    script_args.append(field_nums)
+    return script_args
 
-def cut(field_nums):
-    os.execl("cut", '', field_nums, filename)
+def get_field_list():
+
+    char = os.read(0,1)
+    first_line = ''
+    
+    while char != '\n':
+        first_line = first_line + char
+        char = os.read(0,1)
+
+    field_names = first_line.split('\t')
+
+    return field_names
+
+def cut(script_args):
+    os.execv("cut-wrapper", script_args)
 
 # MAIN
 
-file = process_args()
-field_nums = get_field_nums(file)
-cut(field_nums)
+process_args()
+script_args = get_script_args()
+cut(script_args)
 
 
 
