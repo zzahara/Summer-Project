c55ec8983ed0100cb4a5775ec76d64c2829be607
zdocena <zahara.docena@gmail.com>
2010-07-13 11:08:18 -0700

    remove old code

--- flat_file/zd_select.py
+++ flat_file/zd_select.py
@@ -44,66 +44,6 @@ def list_to_dict(log_data, field_list):
 
     return return_dict
 
-
-
-def process_args1():
-    global argv
-    parser.add_option("-f", action="append", dest="fields")
-    parser.add_option("-v", action="append", dest="values")
-
-    (options, args) = parser.parse_args(argv)
-
-    return options
-
-
-def process_file1(options):
-    field_list = get_field_list()
-  
-    values = parse_values(options)
-
-    for log_line in sys.stdin:
-        log_data = log_line.split('\t')
-
-        if include(log_data, field_list, values):
-            print_values(log_data, field_list, options)         
-
-def print_values1(log_data, field_list, options):
-    for field in options.fields:
-        index = field_list.index(field)
-        print log_data[index] + '\t',
-    print ''
-
-def include1(log_data, field_list, values):
-    for val in values:
-        index = field_list.index(val[0])
-
-        if log_data[index] != val[1]:
-           return False
-           
-    return True          
-
-def parse_values1(options):
-    values = []
-    
-    for x in options.values:
-        print x
-        split = x.split('=')
-        values.append((split[0], split[1]))
-
-    print values     
-    return values
-
-def is_num(string):
-    float_pattern = "[0-9]+\.[0-9]+"
-    int_pattern = "[0-9]+"
-    
-    c_float = re.compile(float_pattern)
-    c_int = re.compile(int_pattern)
-    
-    if c_int.match(string) or c_float.match(string):
-        return True
-    return False
-
 def get_field_list():
     first_line = sys.stdin.readline()
     first_line = first_line.rstrip('\n')
