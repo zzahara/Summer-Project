de0c4c8adf07e7aa6a71971fd43d27dc34d58a0c
zdocena <zahara.docena@gmail.com>
2010-07-13 11:30:13 -0700

    encapsulated in a module

--- web/analytics-non-j.js
+++ web/analytics-non-j.js
@@ -1,60 +1,63 @@
+archive_analytics.namespace("non_jquery")
+archive_analytics.non_jquery.myModule = function() {
 
-var startTime = new Date();
-var loadTime = 0.0;
-img_src = "http://analytics.archive.org/0.gif"
+    var startTime = new Date();
+    var loadTime = 0.0;
+    img_src = "http://analytics.archive.org/0.gif"
 
-function addLoadEvent(func) {
-    var oldOnload = window.onload;
+    function addLoadEvent(func) {
+        var oldOnload = window.onload;
 
-    if (typeof window.onload != 'function') {
-        window.onload = func;
-    } else {
-        window.onload = function() {
-            func()
-            oldOnload();
-        }   
-    }
-};
+        if (typeof window.onload != 'function') {
+            window.onload = func;
+        } else {
+            window.onload = function() {
+                func()
+                oldOnload();
+            }   
+        }
+    };
 
-addLoadEvent(function() {
-    var endTime = new Date();
-    values = []
-    values['loadTime'] = ((endTime.getTime() - startTime.getTime())/100)/10;
+    addLoadEvent(function() {
+        var endTime = new Date();
+        values = []
+        values['loadTime'] = ((endTime.getTime() - startTime.getTime())/100)/10;
 
-    // Get field values    
-    values['timeDiff'] = (new Date().getTimezoneOffset()/60)*(-1); 
-    values['locale'] = get_locale();
-    values['referrer'] = document.referrer
+        // Get field values    
+        values['timeDiff'] = (new Date().getTimezoneOffset()/60)*(-1); 
+        values['locale'] = get_locale();
+        values['referrer'] = document.referrer
 
-    string = format_bug(values);
+        string = format_bug(values);
 
-    loadtime_img = new Image(100,25);
-    loadtime_img.src = img_src + "?" + string;
-});
+        loadtime_img = new Image(100,25);
+        loadtime_img.src = img_src + "?" + string;
+    });
 
 
-function format_bug(values) {
-    ret = []
-    for (var data in values)
-      ret.push(encodeURIComponent(data) + "=" + encodeURIComponent(values[data]));
+    function format_bug(values) {
+        ret = []
+        for (var data in values)
+          ret.push(encodeURIComponent(data) + "=" + encodeURIComponent(values[data]));
 
-    return ret.join("&")
-};
+        return ret.join("&")
+    };
 
-function get_locale() {
-    if (navigator) {
-        if (navigator.language)
-            return navigator.language;
-            
-        else if (naviagtor.browserLanguage)
-            return navigator.browserLanguage;
+    function get_locale() {
+        if (navigator) {
+            if (navigator.language)
+                return navigator.language;
+                
+            else if (naviagtor.browserLanguage)
+                return navigator.browserLanguage;
 
-        else if (navigator.systemLanguage) 
-            return navigator.systemLanguage;
+            else if (navigator.systemLanguage) 
+                return navigator.systemLanguage;
 
-        else if (navigator.userLanguage)
-            return navigator.userLanguage;
-    }
+            else if (navigator.userLanguage)
+                return navigator.userLanguage;
+        }
 
-};
+    };
+}();
 
