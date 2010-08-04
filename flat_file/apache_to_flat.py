d13b9e3fc9c6b6d568829bb528bec240c2a64af8
zdocena <zahara.docena@gmail.com>
2010-08-04 12:27:47 -0700

    added documentation

--- flat_file/apache_to_flat.py
+++ flat_file/apache_to_flat.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 # Written by Zahara Docena
 
+# Input: log file
+# Output: flat file (first line = field names, each line after lists the field values for that log line)
+
+# Example:
+# ip    page    referrer    loadtime    server_ms   timestamp   locale  useragent
+# 0.29.113.149	http://www.yoursite.org http://www.google.com 579.0 en-us 03/Aug/2010:00:01:02    en-us   (compatible; MSIE 8.0; Windows NT 6.0; WOW64; Trident/4.0; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.5.21022; .NET CLR 3.5.30729; MDDC; .NET CLR 3.0.30729; InfoPath.1; .NET4.0C)
+# 0.17.457.261  http://www.yoursite.org http://www.google.com 579.0 en-us 03/Aug/2010:00:01:02    en-us   -
+# (log line values)
+# (log line values)
+# ...
+
+# Note: A '-' in the place of a field value like above represents no value found (in example above: no useragent found)
+
 import re
 import sys
 import cgi
@@ -42,6 +55,9 @@ def process_file(file, img_src, options):
             bug_values = get_bug_values(page_data['bug'], img_src)
             page_data.update(bug_values)
 
+            # count = the number of bug values that should be in the bug
+            # bug values are separated by '&'
+            # ampersands in the referrer bug value will result in a miscount
             if 'count' in bug_values and int(bug_values['count']) == len(bug_values):
                 print_page_values(page_data, bug_values, options.fields)
             else:
@@ -58,6 +74,8 @@ def process_file(file, img_src, options):
     sys.stderr.write("regex didn't match: " + str(regex_no_match) + '\n')
     sys.stderr.write("total lines = " + str(total_lines) + '\n')
 
+# parse the values the analytics.js bug generated
+# return values in a dictionary (associated array)
 def get_bug_values(bug, img_src):
     bug_values = cgi.parse_qs(urlparse.urlparse(bug)[4])
 
@@ -66,34 +84,37 @@ def get_bug_values(bug, img_src):
 
     return bug_values
 
+# prints the first line of the flat file
 def print_fields(fields):
-    for i in range(0, len(fields)):
-        if i != len(fields)-1:
-            print fields[i] + '\t',
-        else:
-            print fields[i]
-            
-def print_page_values(page_data, bug_values, fields):
+    print '\t'.join(fields)
 
-    for i in range(0, len(fields)):
-        field = fields[i]
-        
+# prints page values separated by tabs           
+def print_page_values(page_data, bug_values, fields):
+    values = []
+    
+    for field in fields:
         if field in page_data and page_data[field].strip() != '':
             if field in bug_values:
                 page_data[field] = urllib.unquote(page_data[field])
 
+            # checks for & removes any interfering chars
             if '\n' in page_data[field]:
                 page_data[field] = page_data[field].replace('\n', '')
 
             if '\t' in page_data[field]:
                 page_data[field] = page_data[field].replace('\t', '')
-            
-            print page_data[field] + '\t',
-        else:
-            print '-' + '\t',
-    print ''     
 
+            if '\r' in page_data[field]:
+                page_data[field] = page_data[field].replace('\r', '')
+
+            values.append(page_data[field])
+            
+        else:
+            values.append('-')
+            
+    print '\t'.join(values) 
 
 options = process_args()
+# 0.gif holds the 1 pixel image that is loaded with the bug values as parameters (during the GET request)
 process_file(sys.stdin, '/0.gif', options)
 
